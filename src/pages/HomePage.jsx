cdb4fc0773b86c2fb73253d6b4261937e3886148
Miriasme <johndoe@example.com>
2025-07-04 13:07:01 +0300

    [ADD] feature:{Apropos.jsx} /Update Homepage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,12 @@ import Hero from '../components/Hero';
 import Deboucher from '../components/Debouche';
 import Domaine from '../components/Domaine';
 import Admission from '../components/Admission';
+import Apropos from '../components/Apropos';
 export default function HomePage() {
   return (
     <main className="flex flex-col gap-8">
       <Hero/>
+      <Apropos/>
       <Deboucher/>
       <Domaine/>
       <Admission/>
