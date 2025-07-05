c04546f2b491aee8d1882c649b128d3c4798d377
david <miharisoadavidfils@gmail.com>
2025-07-05 16:08:32 +0300

    [UPDATE] techSection (resize icons, add color themes)

--- src/components/TechSection.jsx
+++ src/components/TechSection.jsx
@@ -10,41 +10,53 @@ import serverless from '../assets/tech/serverless.png';
 import react from '../assets/tech/react.png';
 import openai from '../assets/tech/openapi.png';
 
-const technologies = [
-  { name: "Java", logo: java },
-  { name: "Python", logo: python },
-  { name: "JavaScript", logo: js },
-  { name: "C", logo: c },
-  { name: "TypeScript", logo: ts },
-  { name: "Docker", logo: docker },
-  { name: "Next.js", logo: nextjs },
-  { name: "AWS", logo: aws },
-  { name: "Serverless", logo: serverless },
-  { name: "React", logo: react },
-  { name: "OpenAPI", logo: openai },
+const technologiesUP = [
+  { logo: java },
+  { logo: python },
+  { logo: js },
+  { logo: c },
+  { logo: ts },
+];
+const technologiesDown = [
+  { logo: docker },
+  { logo: nextjs },
+  { logo: aws },
+  { logo: serverless },
+  { logo: react },
+  { logo: openai },
 ];
 
 export default function TechSection() {
   return (
-    <section className="bg-gray-100 py-16 px-6">
+    <section className="bg-gray-100 py-16 px-6 text-[var(--primary)] max-w-4xl mx-auto">
       <div className="text-center mb-12">
         <h2 className="text-3xl font-bold text-primary mb-4">
           Les technos et langages utilisées
         </h2>
-        <p className="text-gray-700 max-w-3xl mx-auto">
+        <p className="max-w-4xl mx-auto text-sm">
           Nos étudiants sont formés à l’utilisation et à la maîtrise des langages de programmation Java et JavaScript. Ces deux langages occupent une place de choix dans le domaine du développement logiciel et du web, offrant une polyvalence et une robustesse appréciées par les développeurs à travers le monde.
         </p>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 justify-items-center">
-        {technologies.map((tech) => (
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 justify-items-center">
+        {technologiesUP.map((tech) => (
           <div key={tech.name} className="flex flex-col items-center space-y-2">
             <img
               src={tech.logo}
               alt={tech.name}
-              className="h-16 object-contain hover:scale-110 transition-transform duration-300"
+              className="h-25 object-contain"
             />
-            <p className="text-sm text-gray-700">{tech.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="mt-2.5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 justify-items-center">
+        {technologiesDown.map((tech) => (
+          <div key={tech.name} className="flex flex-col items-center space-y-2">
+            <img
+              src={tech.logo}
+              alt={tech.name}
+              className="h-12 object-contain"
+            />
           </div>
         ))}
       </div>
